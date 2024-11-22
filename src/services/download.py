963e566e8388b24c00ec7de4f26423479efc142f
heitzjulien <heitzjulien@pm.me>
2024-11-22 08:50:03 +0100

    feat: add download from gdrive function

---
+++ src/services/download.py
@@ -0,0 +1,13 @@
+import gdown
+
+def download_from_gdrive(url, output=None):
+    try:
+        file_id = url.split('/d/')[1].split('/')[0]
+        direct_url = f'https://drive.google.com/uc?id={file_id}'
+
+        downloaded_file = gdown.download(direct_url, output, quiet=False)
+        return downloaded_file
+
+    except Exception as e:
+        print(f"Error while downloading: {str(e)}")
+        return None
