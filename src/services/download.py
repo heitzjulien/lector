803706b57a04f031472b4951ae746932f5ee69d4
heitzjulien <heitzjulien@pm.me>
2024-11-22 09:59:17 +0100

    feat: add convert function pdf to text

--- src/services/download.py
+++ src/services/download.py
@@ -1,6 +1,8 @@
 import gdown
 
+
 def download_from_gdrive(url, output=None):
+    """Download the PDF document in Google Drive."""
     try:
         file_id = url.split('/d/')[1].split('/')[0]
         direct_url = f'https://drive.google.com/uc?id={file_id}'
