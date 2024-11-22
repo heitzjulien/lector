be8bb1681ba6baf00ee27444223ca1f99d402384
heitzjulien <heitzjulien@pm.me>
2024-11-22 08:49:41 +0100

    feat: add the constant file

---
+++ src/utils/const.py
@@ -0,0 +1,15 @@
+from openai import OpenAI
+
+LLM="llama3.2"
+FILE_NAME="MARCUS_AURELIUS.pdf"
+
+PINK = '\033[95m'
+CYAN = '\033[96m'
+YELLOW = '\033[93m'
+NEON_GREEN = '\033[92m'
+RESET_COLOR = '\033[0m'
+
+CLIENT = OpenAI(
+    base_url='http://ollama:11434/v1',
+    api_key=LLM
+)
